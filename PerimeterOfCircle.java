32b2b828c6f2010d34a436bebf21046616387062
jobyojose <92086109+jobyojose@users.noreply.github.com>
2021-11-02 15:55:46 +0530

    Update PerimeterOfCircle.java
    added new class to compute PerimeterOfCircle

--- PerimeterOfCircle.java
+++ PerimeterOfCircle.java
@@ -1,1 +1,22 @@
-//new class PerimeterOfCircle
+import java.util.Scanner;
+
+//added by Joby Jose on 2nd Nov 2021
+// new class PerimeterOfCircle
+class PerimeterOfCircle
+{
+      
+   public static void main(String args[]) 
+    {   
+           
+	   	   Scanner s= new Scanner(System.in);        
+           System.out.println("Enter the radius:");
+           double r= s.nextDouble();         
+           double  a=PerimeterOfCircle.area(r);          
+           System.out.println("Perimeter of Circle is: " +a);   
+      }
+     public static double area(double r)
+    {          
+	   double  a=(22*2*r)/7; 
+           return a;
+    } 
+}
